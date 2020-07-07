b8d82c16d96fd9b6a2a06413fc8d6b23380fc5cc
Alena Rybakina <58230554+Alena0704@users.noreply.github.com>
2020-07-07 16:07:36 +0300

    Update utils.py
    Перепутала с другим файлом

--- csv/root_utils/bmn/utils.py
+++ csv/root_utils/bmn/utils.py
@@ -1,5 +1,5 @@
-import pandas as pd #что-то было про оптимизацию
-import numpy as np #что-то было про оптимизацию
+import pandas as pd 
+import numpy as np 
 import os
 
 from tqdm import tqdm
@@ -7,16 +7,15 @@ from typing import (
     Union,
     Tuple
 )
-from ROOT import (#переписан на C
+from ROOT import (
     TTree,
     TFile
 )
 from  itertools import chain
 import uproot
-from multiprocessing import Process
 
 
-def trackparams2pandas(f,tree: str,branch: str, fname: str) -> pd.DataFrame:
+def trackparams2pandas(f,tree: str,branch: str) -> pd.DataFrame:
     f_1 = f[tree][branch]
     lst=[]
     columns = ['event', 'track','start_x', 'start_y', 'start_z', 'px', 'py', 'pz', 'pt', 'pdg_code', 'parent']
@@ -78,23 +77,25 @@ def trackparams2pandas(f,tree: str,branch: str, fname: str) -> pd.DataFrame:
     arr = np.asarray(arr)
     rn=list(chain.from_iterable(arr))
     lst.append(rn)
-    
-    df = pd.DataFrame(data=np.array(lst).transpose(), columns = columns)\
-           .astype({'event': np.int32, 
-                    'track': np.int32,
-                    'start_x': np.float32,
-                    'start_y': np.float32,
-                    'start_z': np.float32,
-                    'px': np.float32,
-                    'py': np.float32,
-                    'pz': np.float32,
-                    'pt': np.float32,
-                    'pdg_code': np.int16,
-                    'parent': np.int32})
-    #dd = dask.dataframe.from_pandas(df, npartitions=11)
-    writer(df, fname)
+    df = pd.DataFrame(data=np.array(lst).transpose(), columns = columns)
+    return df
 
-def mc2pandas(tree: TTree, attr_name: str, fname: str) -> pd.DataFrame:
+def mc2pandas_for(event_id, e, attr_name):
+    MCs = []
+    for mc in getattr(e, attr_name):
+            MCs.append([event_id,
+                        mc.GetTrackID(), 
+                        mc.GetXIn(), 
+                        mc.GetYIn(), 
+                        mc.GetZIn(), 
+                        mc.GetXOut(), 
+                        mc.GetYOut(), 
+                        mc.GetZOut(), 
+                        mc.GetStation(),
+                        mc.GetModule()])
+    return MCs
+
+def mc2pandas(tree: TTree, attr_name: str) -> pd.DataFrame:
     MCs = []
 
     for event_id, e in tqdm(enumerate(tree)):
@@ -125,10 +126,9 @@ def mc2pandas(tree: TTree, attr_name: str, fname: str) -> pd.DataFrame:
                     'z_out': np.float32,
                     'station': np.int8,
                     'module': np.int8})
-    #dd = dask.dataframe.from_pandas(df, npartitions=10)
-    writer(df, fname)
+    return df
 
-def hits2pandas(tree: TTree, attr_name: str, fname: str) -> pd.DataFrame:
+def hits2pandas(tree: TTree, attr_name: str) -> pd.DataFrame:
     hits = []
     for event_id, e in tqdm(enumerate(tree)):
         for hit in getattr(e, attr_name):
@@ -149,46 +149,36 @@ def hits2pandas(tree: TTree, attr_name: str, fname: str) -> pd.DataFrame:
                     'z': np.float32,
                     'station': np.int8,
                     'module': np.int8})
-    #dd = dask.dataframe.from_pandas(df, npartitions=6)
-    writer(df, fname)
+    return df
 
-def writer(df: pd.DataFrame, fname: str, encoding: str = 'utf-8', 
-             sep: str = '\t') -> None:
-    print(f"Save data to the `{fname}``")
-    df.to_csv(fname, encoding=encoding, sep=sep)
-    print("Complete!")
 
-def root2pandas (fname_out: str, 
-                fname_params_out: str,
-                fname: str, 
+def root2pandas(fname: str, 
                 tree_name: str ='cbmsim', 
                 hit_obj_name: str ='BmnGemStripHit', 
                 mc_obj_name: str ='StsPoint',
-                track_params_obj_name: str ='MCTrack'
-                                                        ) -> None: 
+                track_params_obj_name: str ='MCTrack') -> Union[
+                                                            pd.DataFrame, 
+                                                            Tuple[pd.DataFrame, pd.DataFrame]
+                                                        ]: 
     print("Read file '%s'" % fname)
     f = TFile(fname)
-    
+    ff = uproot.open(fname)
     tree = f.Get(tree_name)
     
     print("File processing...")
     if tree.GetBranch(mc_obj_name):
-       ff = uproot.open(fname)
-       p = Process(target=mc2pandas, args=(tree, mc_obj_name, fname_out,))
-       #mc2pandas(tree, mc_obj_name, fname_out)
-       p1 = Process(target=trackparams2pandas, args=(ff,tree_name,track_params_obj_name, fname_params_out,))
-       #trackparams2pandas(ff,tree_name,track_params_obj_name, fname_params_out)
-       p.start()
-       p1.start()
-       p.join()
-       p1.join()
-       
+        result = (
+            mc2pandas(tree, mc_obj_name),
+            trackparams2pandas(ff,tree_name,track_params_obj_name)
+            #trackparams2pandas(tree, track_params_obj_name)
+            
+        )
     # if file with hits
     elif tree.GetBranch(hit_obj_name):
-        hits2pandas(tree, hit_obj_name, fname_out)
+        result = hits2pandas(tree, hit_obj_name)
     else:
         raise ValueError("File format is not supported. None of the branches "
                          f"[{hit_obj_name}, {mc_obj_name}] exists")
 
     print("Complete!")
-    #return result    
+    return result    
